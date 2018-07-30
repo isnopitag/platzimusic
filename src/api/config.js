8e579470e2c0451b1c8da3a808c438d0bf67d186
Carmona <13030617@itcelaya.edu.mx>
2018-07-29 22:24:50 -0500

    video 11

---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+//Application name 	platzimusic
+//API key 	13df3fb58637b268cf1594cd9dfdd02b
+//Shared secret 	264027386e67f6de454d82336b92538b
+//Registered to 	user4today2807
+export default{
+    appName: 'platzimusic',
+    apiKey: '13df3fb58637b268cf1594cd9dfdd02b',
+    secret: '264027386e67f6de454d82336b92538b',
+    registeredTo:'user4today2807'
+}
